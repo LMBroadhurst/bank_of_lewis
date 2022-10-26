06c9c7706dee77e3d8acd9d485b2628b1cb854b5
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-10-26 15:39:05 +0100

    Init commit

---
+++ BOL/src/test/java/bank_of_lewis/BOL/BolApplicationTests.java
@@ -0,0 +1,13 @@
+package bank_of_lewis.BOL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/bank_of_lewis/BOL/BolApplication.java
@@ -0,0 +1,13 @@
+package bank_of_lewis.BOL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolApplication.class, args);
+	}
+
+}
