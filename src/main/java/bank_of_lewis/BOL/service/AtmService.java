edce3ccf6bc534b0e1434e640c71999dd21dcd92
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-10-27 21:24:01 +0100

    Refactored to improve readability

--- src/main/java/bank_of_lewis/BOL/service/AtmService.java
+++ src/main/java/bank_of_lewis/BOL/service/AtmService.java
@@ -55,13 +55,50 @@ public class AtmService {
 
     public String dispenseNotes (Long id, int cashRequired) {
         Atm atm = atmRepo.getById(id);
+        int numberOf20s = 0;
+        int numberOf50s = Math.floorDiv(cashRequired, 50);
 
         if (cashRequired > atm.calculateTotalCash()) {
             return "No way sire";
         }
 
 //        Checking for "perfect match"
-        int numberOf50s = Math.floorDiv(cashRequired, 50);
+        if (!check50sPerfectMatch(numberOf50s, cashRequired, atm).equals("No match")) {
+            return check50sPerfectMatch(numberOf50s, cashRequired, atm);
+        }
+//        Checking there are enough 50s to meet the demand
+        else if (numberOf50s > 0 && cashRequired % 50 == 0 && numberOf50s > atm.getNote50()) {
+            numberOf50s = atm.getNote50();
+        }
+
+//        If $50 denominations cannot be matched, move to combo of $50 and $20
+//        If more than 1 50, checking if removing a 50 can solve the math
+        if (!check20s50sCombination(cashRequired, numberOf50s, numberOf20s, atm).equals("No match")) {
+            return check20s50sCombination(cashRequired, numberOf50s, numberOf20s, atm);
+        }
+
+//        Final check to see if multiple of $20 can solve math
+        if (!checkMultiples20(cashRequired, atm).equals("No match")) {
+            return checkMultiples20(cashRequired, atm);
+        }
+
+        return "Cannot dispense this value. Please try another. 5.";
+    }
+
+    public String addNotes(Long id, CashToAdd cashToAdd) {
+        Atm atm = atmRepo.getById(id);
+        int note20sToAdd = cashToAdd.getNote20s();
+        int note50sToAdd = cashToAdd.getNote50s();
+
+        atm.setNote20(atm.getNote20() + note20sToAdd);
+        atm.setNote50(atm.getNote50() + note50sToAdd);
+
+        atmRepo.save(atm);
+
+        return atm.getNote20().toString() + " " + atm.getNote50().toString();
+    }
+
+    public String check50sPerfectMatch(int numberOf50s, int cashRequired, Atm atm) {
         if (numberOf50s > 0 && cashRequired % 50 == 0 && numberOf50s < atm.getNote50()) {
             int note50sAfterDispense = atm.getNote50() - numberOf50s;
             atm.setNote50(note50sAfterDispense);
@@ -70,13 +107,13 @@ public class AtmService {
             System.out.println(atm.getNote20().toString() + " - " + atm.getNote50().toString());
             return "0 $20 notes dispensed. " + numberOf50s + " $50 notes dispensed. 1";
         }
-        else if (numberOf50s > 0 && cashRequired % 50 == 0 && numberOf50s > atm.getNote50()) {
-            numberOf50s = atm.getNote50();
-        }
 
+        return "No match";
+    }
+
+    public String check20s50sCombination(int cashRequired, int numberOf50s, int numberOf20s, Atm atm) {
 //        If $50 denominations cannot be matched, move to combo of $50 and $20
         int cashLeftToDispense = cashRequired - (numberOf50s * 50);
-        int numberOf20s = 0;
         if (numberOf50s > 0 && cashLeftToDispense % 20 == 0 && cashLeftToDispense > 0) {
             numberOf20s = cashLeftToDispense / 20;
             atm.setNote50(atm.getNote50() - numberOf50s);
@@ -101,7 +138,10 @@ public class AtmService {
             return numberOf20s + " $20 notes dispensed. " + numberOf50s + " $50 notes dispensed. 3";
         }
 
-//        Final check to see if multiple of $20
+        return "No match";
+    }
+
+    public String checkMultiples20(int cashRequired, Atm atm) {
         if (cashRequired % 20 == 0 && atm.totalInNote20() > cashRequired) {
             int note20sToDispense = cashRequired / 20;
             atm.setNote20(atm.getNote20() - note20sToDispense);
@@ -111,22 +151,8 @@ public class AtmService {
             return note20sToDispense + " $20 notes dispensed." + " 0 $50 notes dispensed. 4";
         }
 
-        atmRepo.save(atm);
-
-        return "Cannot dispense this value. Please try another. 5.";
+        return "No match";
     }
 
-    public String addNotes(Long id, CashToAdd cashToAdd) {
-        Atm atm = atmRepo.getById(id);
-        int note20sToAdd = cashToAdd.getNote20s();
-        int note50sToAdd = cashToAdd.getNote50s();
-
-        atm.setNote20(atm.getNote20() + note20sToAdd);
-        atm.setNote50(atm.getNote50() + note50sToAdd);
-
-        atmRepo.save(atm);
-
-        return atm.getNote20().toString() + " " + atm.getNote50().toString();
-    }
 
 }
