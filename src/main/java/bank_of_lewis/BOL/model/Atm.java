ba2f3eb127c25053233d868bb877fe6e09a6f67e
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-10-27 15:13:18 +0100

    Adding basic SQL for atms

--- src/main/java/bank_of_lewis/BOL/model/Atm.java
+++ src/main/java/bank_of_lewis/BOL/model/Atm.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Atm {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
